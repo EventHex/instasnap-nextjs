59bd4e99d007a963a2ce75a7a29bde35cdc4e1eb
safa saleem o <safasaleem916@gmail.com>
2025-04-28 18:22:07 +0530

    Enhance Home Page Layout and Functionality
    - Added new UI elements including a header section with a title and button.
    - Implemented a message display for when no highlights are found, including an image and explanatory text.
    - Updated asset imports to include new icons and images.
    - Removed unused icon file to clean up assets.

--- app/assets/index.js
+++ app/assets/index.js
@@ -12,7 +12,10 @@ import User from "./icons/Vector (13).svg";
 import HumanSelfi from "./image/Face-Id--Streamline-Tokyo.svg";
 import Sparkle from './icons/Vector (15).svg'  
 import FaceIdBase from './image/Face ID base blue 1.svg'
-import ShineAI from './icons/Vector.svg'
+import ShineAI from './icons/Vector(16).svg'
+import Frame from './image/Frame.svg'
+import EventHex from './icons/Vector (90).svg'
+
 export {
   Logo,
   Phone,
@@ -29,4 +32,6 @@ export {
   Sparkle,
   FaceIdBase,
   ShineAI,
+  Frame,
+  EventHex,
 };
--- app/components/aiInput/index.jsx
+++ app/components/aiInput/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image from 'next/image'
-import { Sparkle } from '../../assets'
 
 const AiInput = ({icon, message, number, label}) => {
   return (
