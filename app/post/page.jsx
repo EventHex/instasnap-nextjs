367a0b9574acc7843edce387d31d3072678ebd39
Muhammed Ajmal CC <muhammedajmalcc6424094@gmail.com>
2025-04-28 23:29:36 +0530

    post page ready

--- app/post/page.jsx
+++ app/post/page.jsx
@@ -11,26 +11,36 @@ const data = [
     id: 1,
     image: Placeholder,
     alt: 'Placeholder',
+    username: 'muhammed ajmal',
+    profile: Placeholder,
   },
   {
     id: 2,
     image: Placeholder,
     alt: 'PotraitPlaceholder',
+    username: 'john doe',
+    profile: Placeholder,
   },
   {
     id: 3,
     image: Placeholder,
     alt: 'Placeholder',
+    username: 'alex jones',
+    profile: Placeholder,
   },
   {
     id: 4,
     image: Placeholder,
     alt: 'PotraitPlaceholder',
+    username: 'jane smith',
+    profile: Placeholder,
   },
   {
     id: 5,
     image: Placeholder,
     alt: 'Placeholder',
+    username: 'kathryn jones',
+    profile: Placeholder,
   },
  
   
@@ -98,9 +108,18 @@ const Page = () => {
         <EventHeader />
       </div>
       
-      <div className="grid grid-cols-1 gap-4">
+      <div className="grid grid-cols-1 mb-[80px] gap-4">
        {data.map((item) => (
         <div key={item.id} className="w-full ">
+          <div className='w-full px-1 py-2 flex gap-2 items-center'>
+            <div className='rounded-full bg-gray-200 w-8 h-8'>
+<Image className='rounded-full w-full h-full' src={item.profile} alt={item.alt} />
+            </div>
+            <div>
+              <p>{item.username}</p>
+            
+            </div>
+          </div>
           <Image src={item.image} alt={item.alt} />
           <div className='w-full flex p-2 justify-between items-center'>
             <div className=''>
