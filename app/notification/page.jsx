91b412f11c8889840e4a25c2296a016c1569eafb
Muhammed Ajmal CC <muhammedajmalcc6424094@gmail.com>
2025-04-29 18:48:17 +0530

    notification page added

--- app/notification/page.jsx
+++ app/notification/page.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import Navbar from '../components/navfooter';
 const NotificationCard = () => {
   return (
-    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden">
-      <div className="p-4 space-y-4">
+    <div className='flex justify-center items-center'>
+    <div className="w-full p-5  rounded-xl  overflow-hidden">
+        <div className='flex justify-center items-center'>
+            <h1 className='text-[15px] font-[600]'>
+                Notification
+            </h1>
+        </div>
+      <div className="space-y-4   mt-5">
+        <div className='p-5 rounded-[16px] border-2 border-gray-200 w-full'>
         <div className="flex items-center space-x-2">
           <div className="w-3 h-3 bg-blue-400 rounded-full"></div>
           <span className="text-xs text-gray-500">16 Nov 2023 | 06:57 PM</span>
@@ -13,10 +20,10 @@ const NotificationCard = () => {
           <h3 className="font-medium text-gray-900">New Photos Uploaded</h3>
           <p className="text-sm text-gray-600">100 new photos has been uploaded to event highlights</p>
         </div>
+        </div>
         
-        <div className="pt-2 border-t border-gray-100">
+        <div className="pt-2 p-5 rounded-[16px] border-2 border-gray-200 w-full  ">
           <div className="flex items-center space-x-2">
-            <div className="w-3 h-3 bg-blue-400 rounded-full"></div>
             <span className="text-xs text-gray-500">16 Nov 2023 | 06:55 PM</span>
           </div>
           
@@ -26,6 +33,8 @@ const NotificationCard = () => {
           </div>
         </div>
       </div>
+    </div>
+    <Navbar/>
     </div>
   );
 };
